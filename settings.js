13de256f5118eee3f9f27dce3453efd946a587b6
YuzkiTodz <yuzakitodz@gmail.com>
2024-10-11 05:49:38 +0700

    rakhale

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6285852038612']
+global.botname = 'Kura Shink'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Rxhl Official"
+global.sticker2 = "🌜"
